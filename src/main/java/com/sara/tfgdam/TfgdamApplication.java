0e33052aaa047946a8f1efca8c4d6721b1d1c0b1
Javier Alcaraz <jam17112001@gmail.com>
2026-02-16 11:25:59 +0100

    Primer commit

---
+++ src/main/java/com/sara/tfgdam/TfgdamApplication.java
@@ -0,0 +1,13 @@
+package com.sara.tfgdam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfgdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfgdamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sara/tfgdam/TfgdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sara.tfgdam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TfgdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
